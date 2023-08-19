3b1bd5b48332c5e10cae743c83923ec203e7e453
Patrick Nguyen <patar.nguyen@gmail.com>
2023-08-18 23:28:41 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Patrick Nguyen on 2023-08-18.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
